e415145ba2c07ffb60c6bd42dd87b302c8023e7a
Riki Singh Khorana <rskhorana@gmail.com>
2019-08-06 05:50:07 -0400

    Add lint to main dir and refactor accordingly

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
  * This is for heroku:
  *   https://devcenter.heroku.com/articles/free-dyno-hours
  */
-setInterval(function() {
+setInterval(() => {
   http.get('http://jenny-wang.herokuapp.com');
 }, 20 * 60 * 1000);
 
@@ -25,15 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 /**
  * API endpoint to return list of projects
  */
-app.get('/api/getProjectList', function(req, res) {
+app.get('/api/getProjectList', (req, res) => {
   res.sendFile(`${__dirname}/public/projects/settings.json`);
 });
 
 /**
  * API endpoint to return list of sources to model images
  */
-app.get('/api/getModelImages', function(req, res) {
-  fs.readdir(`${__dirname}/public/modeling`, function(err, items) {
+app.get('/api/getModelImages', (req, res) => {
+  fs.readdir(`${__dirname}/public/modeling`, (err, items) => {
     const result = items.filter(item => isImage(item));
     res.json(result);
   });
@@ -42,8 +42,8 @@ app.get('/api/getModelImages', function(req, res) {
 /**
  * API endpoint to return list of sources to photography images
  */
-app.get('/api/getPhotographyImages', function(req, res) {
-  fs.readdir(`${__dirname}/public/photography`, function(err, items) {
+app.get('/api/getPhotographyImages', (req, res) => {
+  fs.readdir(`${__dirname}/public/photography`, (err, items) => {
     const result = items.filter(item => isImage(item));
     res.json(result);
   });
@@ -52,13 +52,13 @@ app.get('/api/getPhotographyImages', function(req, res) {
 /**
  * Fallback for all other accesses
  */
-app.get('*', function(req, res) {
+app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`);
 });
 
 /**
  * Listen
  */
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
