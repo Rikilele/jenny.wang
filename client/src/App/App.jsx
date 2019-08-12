402f8dabe43410bd29c5f0917617efae23ce5d74
Riki Singh Khorana <rskhorana@gmail.com>
2019-08-12 13:41:25 -0400

    Remove unnecessary div from App

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -10,15 +10,13 @@ import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        {/*<Route exact path="/projects" component={Projects} />*/}
-        {/*<Route path="/projects/details/:id" component={Project} />*/}
-        {/*<Route path="/photography" component={Photography} />*/}
-        <Route path="/modeling" component={Modeling} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/projects" component={Projects} />
+      <Route path="/projects/details/:id" component={Project} />
+      <Route path="/photography" component={Photography} />
+      <Route path="/modeling" component={Modeling} />
+      <Route path="/contact" component={Contact} />
+    </Switch>
   );
 }
